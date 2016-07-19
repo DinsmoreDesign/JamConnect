07a99fb73bb2076de8b37eddb23adca912db43c8
Derek <derek@dinsmore-design.com>
2016-07-19 14:05:36 -0700

    Adding Transition Animations
    * Adding _uihooks to animate up votes, deletions and page changes
    * Adding fade transitions between separate “pages”
    * Adding movement to posts when posts are up voted or deleted

---
+++ client/templates/application/layout.js
@@ -0,0 +1,20 @@
+Template.layout.onRendered(function() {
+
+  this.find('#main')._uihooks = {
+
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
+    }
+
+  }
+
+});
