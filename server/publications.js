17ce1497d497602e0335b4efeee3c357febb160c
Derek <derek@dinsmore-design.com>
2016-07-19 11:32:46 -0700

    Adding Notifications for New Replies
    Adding Notifications section on nabber that fills when a user receives
    a reply to his or her post

--- server/publications.js
+++ server/publications.js
@@ -10,3 +10,9 @@ Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
   
 });
+
+Meteor.publish('notifications', function() {
+
+  return Notifications.find({userId: this.userId, read: false});
+  
+});
