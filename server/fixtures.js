854fd99a33d9c460609d432481d17c10d0fddf2f
Derek <derek@dinsmore-design.com>
2016-07-27 10:22:46 -0700

    Changed Posts pages & Item styling
    * Added Font Awesome package
    * Removed “Discuss” button from homepage, replaced with “External Link”
    icon/button if URL exists
    * Removed frontpage link on title to external link, replaced with post
    page link
    * Removed old fixtures and replaced with single “Welcome” post
    * Added Split on Newlines package to keep paragraph formatting on post
    pages
    * Styled post pages slightly for more streamlined look

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,79 +3,97 @@ if (Posts.find().count() === 0) {
 
   var now = new Date().getTime();
 
-  // create two users
-  var tomId = Meteor.users.insert({
-    profile: { name: 'Tom Coleman' }
+  var adminId = Meteor.users.insert({
+    profile: { name: 'Admin' }
   });
 
-  var tom = Meteor.users.findOne(tomId);
-
-  var sachaId = Meteor.users.insert({
-    profile: { name: 'Sacha Greif' }
-  });
-
-  var sacha = Meteor.users.findOne(sachaId);
-
-  var telescopeId = Posts.insert({
-    title: 'Introducing Telescope',
-    userId: sacha._id,
-    author: sacha.profile.name,
-    url: 'http://sachagreif.com/introducing-telescope/',
-    submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 2,
-    upvoters: [],
-    votes: 0
-  });
-
-  Comments.insert({
-    postId: telescopeId,
-    userId: tom._id,
-    author: tom.profile.name,
-    submitted: new Date(now - 5 * 3600 * 1000),
-    body: 'Interesting project Sacha, can I get involved?'
-  });
-
-  Comments.insert({
-    postId: telescopeId,
-    userId: sacha._id,
-    author: sacha.profile.name,
-    submitted: new Date(now - 3 * 3600 * 1000),
-    body: 'You sure can Tom!'
-  });
+  var admin = Meteor.users.findOne(adminId);
 
   Posts.insert({
-    title: 'Meteor',
-    userId: tom._id,
-    author: tom.profile.name,
-    url: 'http://meteor.com',
-    submitted: new Date(now - 10 * 3600 * 1000),
+    title: 'Welcome to JamConnect!',
+    userId: admin._id,
+    author: admin.profile.name,
+    url: '',
+    message: 'Welcome to JamConnect, we are glad you found us! If you have any questions, please feel free to ask in this thread and we will be happy to help.',
+    submitted: new Date(now),
     commentsCount: 0,
     upvoters: [],
-    votes: 0
+    votes: 9999
   });
 
-  Posts.insert({
-    title: 'The Meteor Book',
-    userId: tom._id,
-    author: tom.profile.name,
-    url: 'http://themeteorbook.com',
-    submitted: new Date(now - 12 * 3600 * 1000),
-    commentsCount: 0,
-    upvoters: [],
-    votes: 0
-  });
+  // // create two users
+  // var tomId = Meteor.users.insert({
+  //   profile: { name: 'Tom Coleman' }
+  // });
 
-  for (var i = 0; i < 10; i++) {
-    Posts.insert({
-      title: 'Test post #' + i,
-      author: sacha.profile.name,
-      userId: sacha._id,
-      url: 'http://google.com/?q=test-' + i,
-      submitted: new Date(now - i * 3600 * 1000 + 1),
-      commentsCount: 0,
-      upvoters: [],
-      votes: 0
-    });
-  }
+  // var tom = Meteor.users.findOne(tomId);
+
+  // var sachaId = Meteor.users.insert({
+  //   profile: { name: 'Sacha Greif' }
+  // });
+
+  // var sacha = Meteor.users.findOne(sachaId);
+
+  // var telescopeId = Posts.insert({
+  //   title: 'Introducing Telescope',
+  //   userId: sacha._id,
+  //   author: sacha.profile.name,
+  //   url: 'http://sachagreif.com/introducing-telescope/',
+  //   submitted: new Date(now - 7 * 3600 * 1000),
+  //   commentsCount: 2,
+  //   upvoters: [],
+  //   votes: 0
+  // });
+
+  // Comments.insert({
+  //   postId: telescopeId,
+  //   userId: tom._id,
+  //   author: tom.profile.name,
+  //   submitted: new Date(now - 5 * 3600 * 1000),
+  //   body: 'Interesting project Sacha, can I get involved?'
+  // });
+
+  // Comments.insert({
+  //   postId: telescopeId,
+  //   userId: sacha._id,
+  //   author: sacha.profile.name,
+  //   submitted: new Date(now - 3 * 3600 * 1000),
+  //   body: 'You sure can Tom!'
+  // });
+
+  // Posts.insert({
+  //   title: 'Meteor',
+  //   userId: tom._id,
+  //   author: tom.profile.name,
+  //   url: 'http://meteor.com',
+  //   submitted: new Date(now - 10 * 3600 * 1000),
+  //   commentsCount: 0,
+  //   upvoters: [],
+  //   votes: 0
+  // });
+
+  // Posts.insert({
+  //   title: 'The Meteor Book',
+  //   userId: tom._id,
+  //   author: tom.profile.name,
+  //   url: 'http://themeteorbook.com',
+  //   submitted: new Date(now - 12 * 3600 * 1000),
+  //   commentsCount: 0,
+  //   upvoters: [],
+  //   votes: 0
+  // });
+
+  // for (var i = 0; i < 10; i++) {
+  //   Posts.insert({
+  //     title: 'Test post #' + i,
+  //     author: sacha.profile.name,
+  //     userId: sacha._id,
+  //     url: 'http://google.com/?q=test-' + i,
+  //     submitted: new Date(now - i * 3600 * 1000 + 1),
+  //     commentsCount: 0,
+  //     upvoters: [],
+  //     votes: 0
+  //   });
+  // }
   
 }
